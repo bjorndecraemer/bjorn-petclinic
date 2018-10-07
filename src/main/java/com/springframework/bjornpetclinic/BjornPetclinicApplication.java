52d8c97e95e8aff46ff2bf7dc323754fb54c1478
beorn <beorn_d_latch@yahoo.com>
2018-10-08 00:05:23 +0200

    Initial add

---
+++ src/main/java/com/springframework/bjornpetclinic/BjornPetclinicApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.bjornpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjornPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BjornPetclinicApplication.class, args);
+    }
+}
