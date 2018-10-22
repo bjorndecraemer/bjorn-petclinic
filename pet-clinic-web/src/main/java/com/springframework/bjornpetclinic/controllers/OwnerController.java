121005f34dfb82faaf14406d8e17953bb757f644
beorn <beorn_d_latch@yahoo.com>
2018-10-22 18:48:08 +0200

    Fixed broken links #45

--- pet-clinic-web/src/main/java/com/springframework/bjornpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/bjornpetclinic/controllers/OwnerController.java
@@ -20,4 +20,8 @@ public class OwnerController {
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
+    @RequestMapping({"/find"})
+    public String findOwners(){
+        return "notImplemented";
+    }
 }
