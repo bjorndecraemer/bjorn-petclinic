27b9b6dce25a3dc9e49cddd68ce3d0b3b5714384
beorn <beorn_d_latch@yahoo.com>
2018-11-11 18:38:51 +0100

    Adding builder pattern to PetType. #56

---
+++ pet-clinic-web/src/test/java/com/springframework/bjornpetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,23 @@
+package com.springframework.bjornpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PetControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void populatePetTypes() {
+    }
+
+    @Test
+    void findOwner() {
+    }
+
+    @Test
+    void initOwnerBinder() {
+    }
+}
